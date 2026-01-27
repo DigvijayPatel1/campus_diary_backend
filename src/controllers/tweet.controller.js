2b7280be9850dd353668e1f10a3b487ac6678000
Digvijay Patel <dpat19122@gmail.com>
2026-01-27 16:24:10 +0530

    added import for comment and like

--- src/controllers/tweet.controller.js
+++ src/controllers/tweet.controller.js
@@ -4,6 +4,8 @@ import { User } from "../models/user.model.js"
 import { ApiError } from "../utils/ApiError.js"
 import { ApiResponse } from "../utils/ApiResponse.js"
 import { asyncHandler } from "../utils/asyncHandler.js"
+import { Comment } from "../models/comment.model.js"
+import { Like } from "../models/like.model.js"
 
 //----Create Tweet----
 const createTweet = asyncHandler( async (req,res)=>{
