3ea6d3f1099ac1a87755dfc92dbf84611d946776
Digvijay Patel <dpat19122@gmail.com>
2026-01-27 16:37:25 +0530

    removed the extra import

--- src/controllers/interview.controller.js
+++ src/controllers/interview.controller.js
@@ -4,7 +4,6 @@ import { Comment } from "../models/comment.model.js"
 import { asyncHandler } from "../utils/asyncHandler.js"
 import { ApiError } from "../utils/ApiError.js"
 import { ApiResponse } from "../utils/ApiResponse.js"
-import { Comment } from "../models/comment.model.js"
 import { Like } from "../models/like.model.js"
 
 //-------Create Interview-------
