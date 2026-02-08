115be7d374bb0ea96822955d864d42d7f89a18a3
Digvijay Patel <dpat19122@gmail.com>
2026-02-08 15:45:13 +0530

    added the import in the admin middleware

--- src/middlewares/admin.middleware.js
+++ src/middlewares/admin.middleware.js
@@ -1,3 +1,5 @@
+import { ApiError } from "../utils/ApiError.js";
+
 export const verifyAdmin = (req, _, next) => {
     if (req.user?.role !== "admin"){
         throw new ApiError(403, "Admin access only")
